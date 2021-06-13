6977d6a270b61bccef62e5689b97f4bee079cb93
amy null <null@amy.gg>
2021-06-13 04:41:39 -0400

    refactor: CLI into usable subcommands

--- cli/src/client/mod.rs
+++ cli/src/client/mod.rs
@@ -1,14 +1,54 @@
 use std::error::Error;
 use std::vec;
 
+use hyper::Body;
 use hyper::{body::HttpBody, Client};
 use hyperlocal::{UnixClientExt, Uri};
 
+#[derive(Debug)]
+pub enum Method {
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete,
+}
+
 pub async fn get(route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
-    // TODO: Error-handling
-    let url = Uri::new("/tmp/squishd.sock", route).into();
+    request(Method::Get, route).await
+}
+
+pub async fn post(route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
+    request(Method::Post, route).await
+}
+
+#[allow(dead_code)]
+pub async fn put(route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
+    request(Method::Put, route).await
+}
+
+#[allow(dead_code)]
+pub async fn patch(route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
+    request(Method::Patch, route).await
+}
+
+#[allow(dead_code)]
+pub async fn delete(route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
+    request(Method::Delete, route).await
+}
+
+pub async fn request(method: Method, route: &'static str) -> Result<String, Box<dyn Error + Send + Sync>> {
+    let url: hyper::http::Uri = Uri::new("/tmp/squishd.sock", route).into();
     let client = Client::unix();
-    let mut response = client.get(url).await?;
+    let mut response = match method {
+        Method::Get => client.get(url).await?,
+        Method::Post => client.request(hyper::Request::post(url).body(Body::empty())?).await?,
+        Method::Put => client.request(hyper::Request::put(url).body(Body::empty())?).await?,
+        Method::Patch => client.request(hyper::Request::patch(url).body(Body::empty())?).await?,
+        Method::Delete => client.request(hyper::Request::delete(url).body(Body::empty())?).await?,
+        #[allow(unreachable_patterns)]
+        _ => panic!("unimplemented method: {:?}", method),
+    };
     let mut body: Vec<u8> = vec![];
     while let Some(next) = response.data().await {
         let chunk = next?;
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,10 +6,37 @@ mod client;
 
 use std::error::Error;
 
+use clap::{App, Arg};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let res = client::get("/containers/list").await?;
-    println!("got value: {}", res);
+    let matches = App::new("squish")
+        .arg(
+            Arg::new("debug")
+                .long("debug")
+                .short('d')
+                .about(""),
+        )
+        .subcommand(App::new("list"))
+        .subcommand(App::new("create"))
+        .get_matches();
+
+    match matches.subcommand_name() {
+        Some("list") => {
+            let res = client::get("/containers/list").await?;
+            println!("got value: {}", res);
+        },
+        Some("create") => {
+            let res = client::post("/containers/create").await?;
+            println!("got value: {}", res);
+        },
+        Some(cmd) => {
+            println!("Unknown subcommand '{}'", cmd);
+        },
+        None => {
+            println!("No subcommand provided :<");
+        },
+    }
 
     Ok(())
 }
