4d336d1d119bca202e9cd6d11fd79fb8a5e6f76c
amy null <null@amy.gg>
2021-07-29 22:28:04 -0400

    refactor: bytes.to_vec()

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -56,10 +56,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .and(warp::post())
         .and(with_state(global_state.clone()))
         .and(warp::body::bytes().map(|bytes: Bytes| {
-            let mut vec: Vec<u8> = vec![];
-            for byte in bytes.iter() {
-                vec.push(*byte);
-            }
+            let vec: Vec<u8> = bytes.to_vec();
             // TODO: Better error handling(?)
             let body = String::from_utf8(vec).expect("squishfile not valid string");
             squishfile::parse_str(&*body).expect("squishfile invalid")
