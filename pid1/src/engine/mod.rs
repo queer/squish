dd86c4bae3403ba814731f554cbfedce6ac32c0e
amy null <null@amy.gg>
2021-08-01 00:52:08 -0400

    feature(pid1): Bindmount helper + target override

--- pid1/src/engine/mod.rs
+++ pid1/src/engine/mod.rs
@@ -4,7 +4,7 @@ use std::os::unix::io::IntoRawFd;
 use std::path::Path;
 use std::process;
 
-use libsquish::squishfile::Squishfile;
+use libsquish::squishfile::{LayerSpec, Squishfile};
 use nix::mount::{mount, MsFlags};
 use nix::unistd::{chdir, chroot, close, dup, dup2};
 
@@ -57,59 +57,9 @@ pub fn setup_and_run_container(
 
     for (layer_name, layer) in squishfile.layers() {
         if layer_name != "alpine" && layer_name != "app" {
-            // Bind-mount squishfile layers
-            println!(">> bindmounting {:?} => {:?}", layer.path(), layer.target());
-            if layer.path().is_none() && layer.version().is_none() {
-                panic!("squishfile: nothing to mount for layer {}!?", layer_name);
-            }
-            let target = match layer.target() {
-                Some(target) => target.clone(),
-                None => {
-                    if layer.path().is_some() {
-                        let target = layer
-                            .path()
-                            .as_ref()
-                            .unwrap()
-                            .replace("../", "")
-                            .replace("./", "");
-                        format!("/app/{}", target)
-                    } else if layer.version().is_none() {
-                        panic!("squishfile no path or version for layer {}", layer_name);
-                    } else {
-                        // TODO
-                        todo!("mounting squish layer at path");
-                    }
-                }
-            };
-            let target = format!("{}/{}", container_path, target);
-            if layer.path().is_none() && layer.version().is_some() && layer.target().is_none() {
-                // TODO
-                todo!("mounting squish layer normally");
-            }
-            let path = layer.path().as_ref().unwrap();
-            let mount_path = Path::new(path);
-            // Yeah this is technically racy, but literally who cares?
-            if mount_path.exists() {
-                let meta = fs::metadata(path)?;
-                if meta.is_dir() {
-                    touch_dir(&target)?;
-                } else if meta.is_file() {
-                    let target_path = Path::new(&target);
-                    // TODO: Do this better
-                    let parent = target_path.parent().unwrap().to_str().unwrap().to_string();
-                    touch_dir(&parent)?;
-                    touch(&target)?;
-                } else {
-                    println!(">> mount is not a directory or file");
-                }
-                bind_mount(
-                    path,
-                    &target,
-                    MsFlags::MS_RDONLY | MsFlags::MS_NOATIME | MsFlags::MS_NOSUID,
-                )?;
-            } else {
-                println!(">> mount didn't exist")
-            }
+            bind_mount_layer::<&str>(&container_path, layer_name, layer, None)?;
+        } else if layer_name == "app" {
+            bind_mount_layer(&container_path, layer_name, layer, Some("/app/"))?;
         }
     }
 
@@ -143,6 +93,83 @@ pub fn setup_and_run_container(
 
     run_in_container(&squishfile);
     println!(">> done!");
+    Ok(())
+}
+
+fn bind_mount_layer<TO>(
+    container_path: &String,
+    layer_name: &String,
+    layer: &LayerSpec,
+    target_override: Option<TO>,
+) -> Result<(), Box<dyn Error>>
+where
+    TO: Into<String>,
+{
+    // Bind-mount squishfile layers
+    println!(">> bindmounting {:?} => {:?}", layer.path(), layer.target());
+    if layer.path().is_none() && layer.version().is_none() {
+        panic!("squishfile: nothing to mount for layer {}!?", layer_name);
+    }
+    let target = match layer.target() {
+        Some(target) => target.clone(),
+        None => {
+            // If path but no target, mount into /app
+            if layer.path().is_some() {
+                let target = layer
+                    .path()
+                    .as_ref()
+                    .unwrap()
+                    .replace("../", "")
+                    .replace("./", "");
+                format!("/app/{}", target)
+            } else if layer.version().is_none() {
+                panic!("squishfile no path or version for layer {}", layer_name);
+            } else {
+                // TODO
+                todo!("mounting squish layer at path");
+            }
+        }
+    };
+    let target = if let Some(target_override) = target_override {
+        let file_name = Path::new(&target)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string();
+        format!("{}/{}", target_override.into(), file_name)
+    } else {
+        target
+    };
+    let target = format!("{}/{}", container_path, target);
+    if layer.path().is_none() && layer.version().is_some() && layer.target().is_none() {
+        // TODO
+        todo!("mounting squish layer normally");
+    }
+    let path = layer.path().as_ref().unwrap();
+    let mount_path = Path::new(path);
+    // Yeah this is technically racy, but literally who cares?
+    if mount_path.exists() {
+        let meta = fs::metadata(path)?;
+        if meta.is_dir() {
+            touch_dir(&target)?;
+        } else if meta.is_file() {
+            let target_path = Path::new(&target);
+            // TODO: Do this better
+            let parent = target_path.parent().unwrap().to_str().unwrap().to_string();
+            touch_dir(&parent)?;
+            touch(&target)?;
+        } else {
+            println!(">> mount is not a directory or file");
+        }
+        bind_mount(
+            path,
+            &target,
+            MsFlags::MS_RDONLY | MsFlags::MS_NOATIME | MsFlags::MS_NOSUID,
+        )?;
+    } else {
+        println!(">> mount didn't exist");
+    }
     Ok(())
 }
 
