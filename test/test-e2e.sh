685a41cd24305f24a5286527f6e14d79c17e358e
amy null <null@amy.gg>
2021-09-24 15:53:31 -0400

    feature: e2e test script

---
+++ test/test-e2e.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+cargo -q build
+
+# Run daemon
+cargo -q run -p daemon &
+sleep 2
+DAEMON=$(pidof daemon)
+
+# Assert no containers running
+CONTAINER_COUNT=$(cargo -q run -p cli -- ps | wc -l)
+CONTAINER_COUNT=$((CONTAINER_COUNT - 1))
+
+if [ $CONTAINER_COUNT -ne 0 ]; then
+  echo "Expected 0 containers to be running, but found $CONTAINER_COUNT"
+  exit 1
+fi
+
+# Start a container
+cargo -q run -p cli -- create test/squishfile.toml > /dev/null
+
+CONTAINER_COUNT=$(cargo -q run -p cli -- ps | wc -l)
+CONTAINER_COUNT=$((CONTAINER_COUNT - 1))
+
+if [ $CONTAINER_COUNT -ne 1 ]; then
+  echo "Expected 1 container to be running, but found $CONTAINER_COUNT"
+  exit 1
+fi
+
+# Stop the container
+CONTAINER_ID=$(cargo -q run -p cli -- ps | grep -v "ID" | awk '{print $1}')
+cargo -q run -p cli -- stop $CONTAINER_ID > /dev/null
+
+# Assert no containers running
+CONTAINER_COUNT=$(cargo -q run -p cli -- ps | wc -l)
+CONTAINER_COUNT=$((CONTAINER_COUNT - 1))
+
+if [ $CONTAINER_COUNT -ne 0 ]; then
+  echo "Expected 0 containers to be running, but found $CONTAINER_COUNT"
+  exit 1
+fi
+
+# Clean up
+kill $DAEMON
+echo "All good! :D"
